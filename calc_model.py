613c3c0f587a76fba5f6997bd1fe1d9137eae874
Vkutti <70446320+Vkutti@users.noreply.github.com>
2020-09-25 19:52:07 -0700

    Initial commit

---
+++ calc_model.py
@@ -0,0 +1,36 @@
+
+def add(num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return num1 + num2
+
+
+def multiply(num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return num1 * num2
+
+
+def subtract(num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return num1 - num2
+
+
+def divide(num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return num1 / num2
+
+
+def exponent(num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return num1 ** num2
+
+
+
+
+
+
+
